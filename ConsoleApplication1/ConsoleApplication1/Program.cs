50e3ab42056274de88835a8251ce46dad1c921b9
mhysto <szajba0013@gmail.com>
2017-04-25 13:01:09 +0200

    Dawid 1c

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,9 +11,11 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hello!");
-            int a;
-            int b;
+            int a=1;
+            int b=2;
             int c;
+            c = a + b;
+
         }
     }
 }
