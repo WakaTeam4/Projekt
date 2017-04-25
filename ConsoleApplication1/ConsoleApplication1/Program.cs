2887ab1ac6a24486c0941476a78aa901ef3e5bf1
mhysto <szajba0013@gmail.com>
2017-04-25 13:20:13 +0200

    Revert "Hubert 1"

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,10 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = 3;
-            int c = 7;
-
+           
         }
     }
 }
